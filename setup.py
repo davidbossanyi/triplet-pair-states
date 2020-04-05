a1c21c9df233bf7757c169163054fed355eefcd7
davidbossanyi <davebossanyi@gmail.com>
2020-04-05 13:28:06 +0100

    create setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(
+    name='tripletpairs',
+
+    version='0.0.1',
+
+    description='Package for simulations of triplet-pair states.',
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/davidbossanyi/triplet-pair-states/',
+
+    author='David Bossanyi',
+
+    author_email='davebossanyi@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Science/Research',
+        
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3 :: Only',
+        
+        'Operating System :: OS Independent'
+    ],
+
+    keywords='optical spectroscopy',
+
+    packages=find_packages(exclude=['docs']),
+
+    python_requires='>=3.7',
+
+    install_requires=['numpy>=1.18.1','scipy>=1.4.1'],
+
+)
